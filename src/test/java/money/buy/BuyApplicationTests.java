dc2cc4d696781a16179c01845f5d920369f2cff3
Nano2998 <gytjs2998@naver.com>
2024-10-30 10:50:24 +0900

    초기 설정

---
+++ src/test/java/money/buy/BuyApplicationTests.java
@@ -0,0 +1,13 @@
+package money.buy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BuyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
