9892ec8af970ae94820b5decc344783563efb724
Nano2998 <gytjs2998@naver.com>
2024-10-31 21:23:25 +0900

    JWT 로그인기능 추가

--- src/main/java/whynotthis/domain/BuyApplication.java
+++ src/main/java/whynotthis/domain/BuyApplication.java
@@ -1,8 +1,10 @@
-package money.buy;
+package whynotthis.domain;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class BuyApplication {
 
--- src/test/java/whynotthis/domain/BuyApplicationTests.java
+++ src/test/java/whynotthis/domain/BuyApplicationTests.java
@@ -1,4 +1,4 @@
-package money.buy;
+package whynotthis.domain;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
